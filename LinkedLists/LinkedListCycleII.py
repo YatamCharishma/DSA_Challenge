b135827e23d9c23cc2c4304dcea63a90d34aa80f
YatamCharishma <93082427+YatamCharishma@users.noreply.github.com>
2023-08-02 22:36:01 +0530

    Create LinkedListCycleII.py

---
+++ LinkedLists/LinkedListCycleII.py
@@ -0,0 +1,19 @@
+# return the node where the cycle begins
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = head
+        fast = head
+        flg = 0
+        while(fast != None and fast.next != None):
+            fast = fast.next.next
+            slow = slow.next
+            if(slow == fast):
+                flg = 1
+                break
+        if(flg==0):
+            return None
+        else:
+            while(head != slow):
+                slow = slow.next
+                head = head.next
+            return head
