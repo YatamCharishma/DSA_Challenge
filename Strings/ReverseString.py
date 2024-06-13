7b9d334d391991be4a43c57bac436301badbcc9a
Yatam Charishma <93082427+YatamCharishma@users.noreply.github.com>
2024-06-13 13:46:21 +0530

    Create ReverseString.py

---
+++ Strings/ReverseString.py
@@ -0,0 +1,13 @@
+#Input: s = ["h","e","l","l","o"]
+#Output: ["o","l","l","e","h"]
+
+def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        left = 0
+        right = len(s) - 1
+        while(left <= right):
+            s[left], s[right] = s[right], s[left]
+            left+=1
+            right-=1
