944bf1ab94acb74170e906574e43aac7bcae4688
Yatam Charishma <93082427+YatamCharishma@users.noreply.github.com>
2024-06-16 19:44:19 +0530

    Create GreatestCommonDivisorofStrings.py

---
+++ Strings/GreatestCommonDivisorofStrings.py
@@ -0,0 +1,9 @@
+#Input: str1 = "ABCABC", str2 = "ABC"
+#Output: "ABC"
+
+from math import gcd
+  def gcdOfStrings(self, str1: str, str2: str) -> str:
+      if(str1+str2 != str2+str1):
+          return ""
+      ans = gcd(len(str1), len(str2))
+      return str1[:ans]
